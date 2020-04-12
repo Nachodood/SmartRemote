ed77db32ecbcb75662962d8bc798c2f856f31117
Nachodood <ncumplido@msn.com>
2020-04-12 17:12:41 +0100

    Implement custom adapter but m_deviceSensorsList issue

--- app/src/main/java/com/example/smartremote/MainActivity.java
+++ app/src/main/java/com/example/smartremote/MainActivity.java
@@ -2,25 +2,62 @@ package com.example.smartremote;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button m_btnConnect,
-            m_btnViewGestures;
+    Button m_btnConnect, m_btnViewGestures;
+
+    TextView m_txtViewAvailableSensors;
+
+    ListView m_listAvailableSensors;
+
+    List<Sensor> m_deviceSensorsList;
+    ArrayList<String> m_StringSensorsList;
+    ScrollListAdapter m_customListAdapter;
+
+    Sensor m_currentSensor;
+    String m_currentSensorString;
+
+    SensorManager sensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        m_deviceSensorsList = sensorManager.getSensorList(Sensor.TYPE_ALL);
+
+        m_StringSensorsList = new ArrayList<>();
+
+        int i=0;
+        while(i<=m_deviceSensorsList.size())
+        {
+            m_currentSensor = m_deviceSensorsList.get(i);
+            m_currentSensorString = m_currentSensor.getName();
+            m_StringSensorsList.add(m_currentSensorString);
+            i++;
+        }
+
+        m_customListAdapter = new ScrollListAdapter(this, m_StringSensorsList);
+
+        m_listAvailableSensors = findViewById(R.id.list_available_sensors);
+        m_listAvailableSensors.setAdapter(m_customListAdapter);
+
         m_btnConnect = findViewById(R.id.btn_connect);
-        m_btnViewGestures = findViewById(R.id.btn_view_gestures);
-
-
         m_btnConnect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -30,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        m_btnViewGestures = findViewById(R.id.btn_view_gestures);
         m_btnViewGestures.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,6 +76,8 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(viewGesturesIntent);
             }
         });
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
     }
 }
---
+++ app/src/main/java/com/example/smartremote/ScrollListAdapter.java
@@ -0,0 +1,38 @@
+package com.example.smartremote;
+
+import android.content.Context;
+import android.database.DataSetObserver;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class ScrollListAdapter extends ArrayAdapter<String> {
+
+    public ScrollListAdapter(@NonNull Context context, ArrayList<String> sensorNames) {
+        super(context, R.layout.sensor_item, sensorNames);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+
+        View sensorItemView = inflater.inflate(R.layout.sensor_item, parent,false);
+
+        String sensorStringItem = getItem(position);
+        TextView sensorNameTextView = sensorItemView.findViewById(R.id.txt_sensor_name);
+
+        sensorNameTextView.setText(sensorStringItem);
+
+        return sensorItemView;
+
+    }
+}
